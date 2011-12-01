a96e5a478b661fac7b1478f18f7e64631edfa6c1
Ray Miller <rm7@sanger.ac.uk>
2011-12-01 14:46:41 +0000

    Add many-to-many user/role relationships.

--- packages/LIMS2-Schema/lib/LIMS2/Schema/Result/Role.pm
+++ packages/LIMS2-Schema/lib/LIMS2/Schema/Result/Role.pm
@@ -113,5 +113,8 @@ __PACKAGE__->has_many(
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+__PACKAGE__->many_to_many( 'users' => 'user_roles' => 'user' );
+
 __PACKAGE__->meta->make_immutable;
 1;
