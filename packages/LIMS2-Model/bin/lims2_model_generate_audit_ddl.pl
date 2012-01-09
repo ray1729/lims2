4ae824af63ead3a8669229e0f34a3cc8ce46b7bb
Ray Miller <rm7@sanger.ac.uk>
2012-01-09 16:41:10 +0000

    Include transaction id in audit tables

--- packages/LIMS2-Model/bin/lims2_model_generate_audit_ddl.pl
+++ packages/LIMS2-Model/bin/lims2_model_generate_audit_ddl.pl
@@ -27,6 +27,7 @@ CREATE TABLE [% audit_schema %].[% table_name %] (
 audit_op CHAR(1) NOT NULL CHECK (audit_op IN ('D','I','U')),
 audit_user TEXT NOT NULL,
 audit_stamp TIMESTAMP NOT NULL,
+audit_txid INTEGER NOT NULL,
 [% column_spec.join(",\n") %]
 );
 GRANT SELECT ON [% audit_schema %].[% table_name %] TO [% ro_role %];
@@ -38,11 +39,11 @@ CREATE OR REPLACE FUNCTION [% main_schema %].process_[% table_name %]_audit()
 RETURNS TRIGGER AS $[% table_name %]_audit$
     BEGIN
         IF (TG_OP = 'DELETE') THEN
-           INSERT INTO [% audit_schema %].[% table_name %] SELECT 'D', user, now(), OLD.*;
+           INSERT INTO [% audit_schema %].[% table_name %] SELECT 'D', user, now(), txid_current(), OLD.*;
         ELSIF (TG_OP = 'UPDATE') THEN
-           INSERT INTO [% audit_schema %].[% table_name %] SELECT 'U', user, now(), NEW.*;
+           INSERT INTO [% audit_schema %].[% table_name %] SELECT 'U', user, now(), txid_current(), NEW.*;
         ELSIF (TG_OP = 'INSERT') THEN
-           INSERT INTO [% audit_schema %].[% table_name %] SELECT 'I', user, now(), NEW.*;
+           INSERT INTO [% audit_schema %].[% table_name %] SELECT 'I', user, now(), txid_current(), NEW.*;
         END IF;
         RETURN NULL;
     END;
