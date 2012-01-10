2bb2a99cd4aa23da28caeed170c033eadef2ba1e
Ray Miller <rm7@sanger.ac.uk>
2012-01-10 12:19:06 +0000

    Override as_string() method to include information about the failed validations

--- packages/LIMS2-Model/lib/LIMS2/Model/Error/Validation.pm
+++ packages/LIMS2-Model/lib/LIMS2/Model/Error/Validation.pm
@@ -18,6 +18,39 @@ has results => (
     required => 1
 );
 
+override as_string => sub {
+    my $self = shift;
+
+    my @errors;
+    my $res = $self->results;
+    
+    if ( $res->has_missing ) {
+        for my $f ( $res->missing ) {
+            push @errors, "$f, is missing";
+        }
+    }
+
+    if ( $res->has_invalid ) {
+        for my $f ( $res->invalid ) {
+            push @errors, "$f, is invalid: " . join q{,}, @{ $res->invalid( $f ) };
+        }
+    }
+
+    if ( $res->has_unknown ) {
+        for my $f ( $res->unknown ) {
+            push @errors, "$f, is unknown";
+        }
+    }
+
+    my $str = join "\n\t", $self->message, @errors;
+
+    if ( $self->show_stack_trace ) {
+        $str .= "\n\n" . $self->stack_trace->as_string;
+    }
+
+    return $str;
+};
+
 __PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 
 1;
