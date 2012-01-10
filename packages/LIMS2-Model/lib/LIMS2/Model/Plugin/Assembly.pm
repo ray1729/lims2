420ade5ed321f2d19adfc8caf7feb99bc9616148
Ray Miller <rm7@sanger.ac.uk>
2012-01-10 12:20:04 +0000

    Implement delete_assembly() method

--- packages/LIMS2-Model/lib/LIMS2/Model/Plugin/Assembly.pm
+++ packages/LIMS2-Model/lib/LIMS2/Model/Plugin/Assembly.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 
 use Moose::Role;
+use Hash::MoreUtils qw( slice );
 use namespace::autoclean;
 
 requires qw( schema check_params throw );
@@ -17,6 +18,30 @@ sub create_assembly {
 
     return $assembly->as_hash;
 }
+
+sub delete_assembly {
+    my ( $self, $params ) = @_;
+
+    my $validated_params = $self->check_params( 'delete_assembly', $params );
+
+    my %search = slice( $validated_params, 'assembly' );
+    my $assembly = $self->schema->resultset( 'Assembly' )->find( \%search )
+        or $self->throw(
+            NotFound => {
+                entity_class  => 'Assembly',
+                search_params => \%search
+            }
+        );
+
+    if ( $validated_params->{casdade} ) {
+        $assembly->bac_clone_loci_rs->delete;
+        $assembly->design_oligo_loci_rs->delete;
+    }
+
+    $assembly->delete;
+
+    return 1;
+}   
 
 sub list_assemblies {
     my ( $self ) = @_;
