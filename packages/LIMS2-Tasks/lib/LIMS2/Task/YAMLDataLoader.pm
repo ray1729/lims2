7e8e3ee50a073067205c82c8c0e3a93d57545cfa
Ray Miller <rm7@sanger.ac.uk>
2012-01-13 15:01:35 +0000

    Improve reporting of number of records processed/skipped

--- packages/LIMS2-Tasks/lib/LIMS2/Task/YAMLDataLoader.pm
+++ packages/LIMS2-Tasks/lib/LIMS2/Task/YAMLDataLoader.pm
@@ -28,29 +28,31 @@ sub execute {
 
     $self->log->info( "Running " . $self->abstract );
 
-    my $total = 0;
+    my ( $total_seen, $total_skipped ) = (0,0);
     
     $self->model->txn_do(
         sub {
             for my $input_file ( @{$args} ) {
                 $self->log->info( "Loading data from $input_file" );
-                my $file_count = 0;
+                my ($file_seen, $file_skipped) = (0,0);
                 my $it = iyaml( $input_file );
                 while ( my $datum = $it->next ) {
+                    $file_seen++;
                     if ( ! $self->wanted( $datum ) ) {
                         $self->log->warn( "Skipping record:\n" . YAML::Any::Dump( $datum ) );
+                        $file_skipped++;
                         next;                        
                     }                    
                     $self->create( $datum );
-                    $file_count++;
-                    if ( $file_count % 100 == 0 ) {
-                        $self->log->info( "Processed $file_count records from $input_file" );
+                    if ( $file_seen % 100 == 0 ) {
+                        $self->log->info( "Processed $file_seen records (skipped $file_skipped)" );
                     }                    
                 }
-                $self->log->info( "File $input_file complete ($file_count records)" );
-                $total += $file_count;                
+                $self->log->info( "Loading data from $input_file complete (saw $file_seen records, skipped $file_skipped)" );
+                $total_seen += $file_seen;
+                $total_skipped += $file_skipped;
             }
-            $self->log->info( "Processed $total records" );
+            $self->log->info( "Processed $total_seen records (skipped $total_skipped)" );
             unless ( $self->commit ) {
                 $self->log->warn( "Rollback" );
                 $self->model->txn_rollback;
