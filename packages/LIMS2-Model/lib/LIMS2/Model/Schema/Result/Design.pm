8819b5621b88c7466ab1144a6808e501efac9c7f
Ray Miller <rm7@sanger.ac.uk>
2012-01-13 15:24:19 +0000

    Metadata changes from latest schema dump

--- packages/LIMS2-Model/lib/LIMS2/Model/Schema/Result/Design.pm
+++ packages/LIMS2-Model/lib/LIMS2/Model/Schema/Result/Design.pm
@@ -206,8 +206,8 @@ __PACKAGE__->has_many(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07014 @ 2012-01-12 13:54:10
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:GBUHXs7pXg0YnmJpn5opEQ
+# Created by DBIx::Class::Schema::Loader v0.07014 @ 2012-01-13 15:23:03
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Sk8sVRAEoUAQOD+ITOKO9A
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
