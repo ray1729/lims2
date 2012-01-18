6e365e9f8ebd26af9974a45466f891efd18f9046
Ray Miller <rm7@sanger.ac.uk>
2012-01-18 17:26:25 +0000

    Add valid_primers_for_well() method

--- packages/LIMS2-HTGT-Migrate/lib/LIMS2/HTGT/Migrate/Utils.pm
+++ packages/LIMS2-HTGT-Migrate/lib/LIMS2/HTGT/Migrate/Utils.pm
@@ -9,14 +9,17 @@ use Sub::Exporter -setup => {
                        format_bac_library
                        format_well_name
                        trim
+                       valid_primers_for_well
                )
              ]
 };
 
+use HTGT::DBFactory;
 use DateTime::Format::Oracle;
 use Scalar::Util qw( blessed );
 use Try::Tiny;
 use DateTime;
+use Log::Log4perl qw( :easy );
 
 sub format_well_name {
     my $well_name = shift;
@@ -64,6 +67,8 @@ sub format_bac_library {
 sub trim {
     my $str = shift;
 
+    $str = '' unless defined $str;
+    
     for ( $str ) {
         s/^\s+//;
         s/\s+$//;
@@ -71,6 +76,49 @@ sub trim {
 
     return $str;
 }
+
+{
+    my $qc_schema;
+    
+    sub valid_primers_for_well {
+        my ( $well, $qctest_result_id ) = @_;
+
+        $qctest_result_id ||= $well->well_data_value( 'qctest_result_id' );
+        unless ( defined $qctest_result_id ) {
+            DEBUG( "no QC test result id for $well" );
+            return {};
+        }
+
+        $qc_schema ||= HTGT::DBFactory->connect( 'vector_qc' );
+                
+        my $qctest_result = $qc_schema->resultset( 'QctestResult' )->find(
+            {
+                qctest_result_id => $qctest_result_id
+            }
+        );
+
+        unless ( $qctest_result ) {
+            DEBUG( "QC test result $qctest_result_id not found" );
+            return {};        
+        }
+
+        my %valid_primers;
+    
+        foreach my $primer ( $qctest_result->qctestPrimers ) {
+            my $seq_align_feature = $primer->seqAlignFeature
+                or next;
+            my $loc_status = $seq_align_feature->loc_status
+                or next;
+            $valid_primers{ uc( $primer->primer_name ) } = 1
+                if $loc_status eq 'ok';
+        }
+
+        DEBUG( "valid primers for $well: " . join( q{, }, keys %valid_primers ) );
+        
+        return join( q{,}, sort keys %valid_primers );
+    }
+}
+
 
 
 
