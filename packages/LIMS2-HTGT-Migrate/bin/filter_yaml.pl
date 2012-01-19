af4afc848eb66a2f3898844722665cc153aa6a8d
Ray Miller <rm7@sanger.ac.uk>
2012-01-19 15:57:43 +0000

    Initial check-in of various data cleanup scripts

---
+++ packages/LIMS2-HTGT-Migrate/bin/filter_yaml.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings FATAL => 'all';
+
+use YAML::Any;
+use LIMS2::Util::YAMLIterator;
+use IO::Handle;
+use Path::Class;
+use File::Temp;
+use Getopt::Long;
+
+GetOptions(
+    'i:s' => \my $inplace
+) and @ARGV == 1 or die "Usage: $0 FILENAME.YAML\n";
+
+my $file = file( shift @ARGV );
+
+my $ofh;
+if ( defined $inplace ) {
+    $ofh = File::Temp->new( DIR => $file->dir )
+        or die "create tmp file: $!";
+}
+else {
+    $ofh = IO::Handle->new->fdopen( fileno(STDOUT), 'w' )
+        or die "fdopen STDOUT: $!";
+}
+
+my %seen;
+
+my $it = iyaml( $file->openr );
+
+while ( my $plate = $it->next ) {
+    for my $well ( values %{ $plate->{wells} } ) {
+        my $parent_well = delete $well->{parent_well};
+        $well->{parent_wells} = [ $parent_well ];
+    }
+    $ofh->print( Dump( $plate ) );
+}
+
+$ofh->close;
+
+if ( defined $inplace and length $inplace ) {
+    rename( $file, $file . $inplace )
+        or die "backup $file: $!";
+}
+
+if ( defined $inplace ) {
+    rename( $ofh->filename, $file )
+        or die "rename tmp file: $!";    
+}
+
