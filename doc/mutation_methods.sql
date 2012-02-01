daeb186dc6837d25483b33f82b161884e4a1ccff
Ray Miller <rm7@sanger.ac.uk>
2012-02-01 11:09:05 +0000

    Initial check-in of workflow and data analysis from vvi

--- doc/mutation_methods.sql
+++ doc/mutation_methods.sql
@@ -4,8 +4,9 @@ CREATE TABLE mutation_methods (
 GRANT SELECT ON mutation_methods TO :ro_role;
 GRANT SELECT, INSERT, UPDATE, DELETE ON mutation_methods TO :rw_role;
 
-INSERT INTO mutation_methods(mutation_method) VALUES ( 'Targeted mutation' );
-INSERT INTO mutation_methods(mutation_method) VALUES ( 'Recombination mediated cassette exchange' );
+INSERT INTO mutation_methods(mutation_method) VALUES
+( 'Targeted mutation' ),
+( 'Recombination mediated cassette exchange' );
 
 CREATE TABLE mutation_types (
        mutation_type        TEXT PRIMARY KEY
@@ -13,11 +14,12 @@ CREATE TABLE mutation_types (
 GRANT SELECT ON mutation_types TO :ro_role;
 GRANT SELECT, INSERT, UPDATE, DELETE ON mutation_types TO :rw_role;
 
-INSERT INTO mutation_types(mutation_type) VALUES ( 'Conditional ready' );
-INSERT INTO mutation_types(mutation_type) VALUES ( 'Deletion' );
-INSERT INTO mutation_types(mutation_type) VALUES ( 'Targeted non-conditional' );
-INSERT INTO mutation_types(mutation_type) VALUES ( 'Cre knock-in' );
-INSERT INTO mutation_types(mutation_type) VALUES ( 'Cre BAC' );
+INSERT INTO mutation_types(mutation_type) VALUES
+( 'Conditional ready' ),
+( 'Deletion' ),
+( 'Targeted non-conditional' ),
+( 'Cre knock-in' ),
+( 'Cre BAC' );
 
 CREATE TABLE mutation_subtypes (
        mutation_subtype     TEXT PRIMARY KEY
@@ -25,10 +27,87 @@ CREATE TABLE mutation_subtypes (
 GRANT SELECT ON mutation_subtypes TO :ro_role;
 GRANT SELECT, INSERT, UPDATE, DELETE ON mutation_subtypes TO :rw_role;
 
-INSERT INTO mutation_subtypes (mutation_subtype) VALUES ( 'Domain disruption' );
-INSERT INTO mutation_subtypes (mutation_subtype) VALUES ( 'Frameshift' );
-INSERT INTO mutation_subtypes (mutation_subtype) VALUES ( 'Artificial Intron' );
-INSERT INTO mutation_subtypes (mutation_subtype) VALUES ( 'Hprt' );
-INSERT INTO mutation_subtypes (mutation_subtype) VALUES ( 'Rosa26' );
-INSERT INTO mutation_subtypes (mutation_subtype) VALUES ( 'CDS' );
-INSERT INTO mutation_subtypes (mutation_subtype) VALUES ( '3'' UTR' );
+INSERT INTO mutation_subtypes (mutation_subtype) VALUES
+( 'Domain disruption' ),
+( 'Frameshift' ),
+( 'Artificial Intron' ),
+( 'Hprt' ),
+( 'Rosa26' ),
+( 'CDS' ),
+( '3'' UTR' );
+
+CREATE TABLE recombinases (
+       recombinase      TEXT PRIMARY KEY
+);
+
+INSERT INTO recombinases(recombinase) VALUES ( 'Cre' ), ( 'Flp' ), ( 'Dre' );
+
+CREATE TABLE recombinase_combinations (
+       recombinase_combination_id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE recombinase_combinations_recombinases (
+       recombinase_combination_id INTEGER NOT NULL REFERENCES recombinase_combinations(recombinase_combination_id),
+       recombinase                TEXT NOT NULL REFERENCES recombinases(recombinase),
+       rank                       INTEGER NOT NULL,
+       PRIMARY KEY(recombinase_combination_id, rank)
+);
+
+CREATE TABLE synthetic_constructs (
+       synthetic_construct_id     SERIAL PRIMARY KEY
+       type
+);
+
+CREATE TABLE intermediate_vectors (
+       synthetic_construct_id   INTEGER PRIMARY KEY REFERENCES synthetic_constructs(synthetic_construct_id),
+       design_id                INTEGER NOT NULL REFERENCES designs( design_id ),
+       cassette                 TEXT NOT NULL,
+       backbone                 TEXT NOT NULL,
+       UNIQUE( design_id, cassette, backbone )
+);
+
+CREATE TABLE final_vectors (
+       synthetic_construct_id     INTEGER PRIMARY KEY REFERENCES synthetic_constructs(synthetic_construct_id),
+       design_id                  INTEGER NOT NULL REFERENCES designs( design_id ),
+       cassette                   TEXT NOT NULL,
+       backbone                   TEXT NOT NULL,
+       recombinase_combination_id INTEGER NOT NULL REFERENCES recombinase_combinations(recombinase_combination_id),
+       UNIQUE( design_id, cassette, backbone, recombinase_combination_id )
+);
+
+-- XXX needs a better name
+CREATE TABLE es_cells (
+       synthetic_construct_id   INTEGER PRIMARY KEY REFERENCES synthetic_constructs(synthetic_construct_id),
+       first_allele_id          INTEGER NOT NULL REFERENCES final_vectors(synthetic_construct_id),
+       second_allele_id         INTEGER REFERENCES final_vectors(synthetic_construct_id),
+       UNIQUE(first_allele, second_allele)
+);
+
+wells:
+ - well_id
+ - plate_id
+ - well_name
+ - expected_synthetic_construct_id
+
+assays:
+ - assay_id - autoincrement
+ - assay_type
+
+assay_lrpcr
+ - assay_id
+ - well_id
+ - synthetic_construct_id
+
+
+assay_loa
+ - assay_id
+ - well_id
+ - synthetic_construct_id
+
+
+ - assay_result [pass, fail]
+
+* Concern:
+  - Assay completed on well on assumption that well has expected synthetic construct
+  - Well is sequenced, observed a different synthetic construct
+  - What does this mean for the previous assay?
