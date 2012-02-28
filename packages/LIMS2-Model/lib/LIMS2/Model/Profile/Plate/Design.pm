92986e9c452cbcde583fa0d9714bf7fe7b6bd06d
Sajith Perera <sajith.perera@gmail.com>
2012-02-28 15:26:00 +0000

    Add new profile for Design plates, moved functions from PCS profile into Plate role

---
+++ packages/LIMS2-Model/lib/LIMS2/Model/Profile/Plate/Design.pm
@@ -0,0 +1,51 @@
+package LIMS2::Model::Profile::Plate::Design;
+use Moose;
+use namespace::autoclean;
+use Const::Fast;
+
+const my @DESIGN_WELL_DATA => qw(
+well_name
+pipeline
+design_id
+design_type
+assay_results
+legacy_qc_results
+assay_pending
+assay_complete
+accepted
+);
+
+with qw( LIMS2::Model::Profile::Plate );
+
+sub as_hash {
+    my $self = shift;
+
+    my $plate_data = $self->_get_plate_data;
+    $plate_data->{wells} = [ map { $self->process_well_design_profile( $_ ) } @{ $self->wells } ];
+    $plate_data->{well_data} =\@DESIGN_WELL_DATA;
+
+    return $plate_data;
+}
+
+sub process_well_design_profile {
+    my ( $self, $well ) = @_;
+    my $well_data = $self->process_default_well( $well );
+
+    my $process = $well->process;
+    my $create_di_process = $self->get_process_of_type( $process, 'create_di' );
+    return $well_data unless $create_di_process; 
+
+    my $design = $well->process->process_create_di->design;
+    $well_data->{design_id}   = $design->design_id;
+    $well_data->{design_type} = $design->design_type;
+
+    $well_data->{assay_results} = $self->get_well_assay_results( $well ); 
+    $well_data->{legacy_qc_results} = $self->get_legacy_qc_results( $well );
+
+    return $well_data;
+}
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
--- packages/LIMS2-Model/lib/LIMS2/Model/Profile/Plate/PCS.pm
+++ packages/LIMS2-Model/lib/LIMS2/Model/Profile/Plate/PCS.pm
@@ -13,6 +13,10 @@ design_id
 design_type
 design_well
 assay_results
+legacy_qc_results
+assay_pending
+assay_complete
+accepted
 );
 
 with qw( LIMS2::Model::Profile::Plate );
@@ -30,7 +34,6 @@ sub as_hash {
 sub process_well_pcs_profile {
     my ( $self, $well ) = @_;
     my $well_data = $self->process_default_well( $well );
-    # must deal with PCS re-arrays
 
     my $process = $well->process;
     my $int_recom_process = $self->get_process_of_type( $process, 'int_recom' );
@@ -44,46 +47,10 @@ sub process_well_pcs_profile {
     $well_data->{cassette}    = $int_recom_process->cassette;
     $well_data->{backbone}    = $int_recom_process->backbone;
 
-    my $assay_results;
-    for my $assay_result ( $well->well_assay_results->all ) {
-        $assay_result .= $assay_result->assay . ' - ' . $assay_result->result . "\n";
-    }
-    $well_data->{assay_results} = $assay_results;
+    $well_data->{assay_results} = $self->get_well_assay_results( $well ); 
+    $well_data->{legacy_qc_results} = $self->get_legacy_qc_results( $well );
 
     return $well_data;
-}
-
-sub get_process_of_type {
-    my ( $self, $process, $type ) = @_;
-
-    if ( $process->process_type->process_type eq $type ) {
-        my $process_type = 'process_' . $type;
-        return $process->$process_type;
-    } 
-    elsif ( $process->process_type->process_type eq 'rearray' ) {
-        my @source_processes = map { $_->source_well->process }
-            $process->process_rearray->process_rearray_source_wells_rs->search( 
-                {}, { prefetch => { source_well => 'process' } } );
-
-        if ( @source_processes == 1 ) {
-            $self->get_process_of_type( shift @source_processes, $type );
-        }
-        else {
-            my @source_params = map { synthetic_construct_params( $_ ) } @source_processes;
-            my $params = shift @source_params;
-            for my $other_params ( @source_params ) {
-                if ( ! Compare( $params, $other_params ) ) {
-                    LIMS2::Model::Error::Database->throw(
-                        'Rearray process ' . $process->process_id 
-                         . ' has source wells containing different constructs'
-                    );                
-                }
-            }
-            $self->get_process_of_type( shift @source_processes, $type );
-        }
-    }
-
-    return;
 }
 
 
