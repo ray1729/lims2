85a9c635035b895164545d2278df4ab7106d598d
Sajith Perera <sajith.perera@gmail.com>
2012-02-28 15:24:44 +0000

    add common functions to Plate profile

--- packages/LIMS2-Model/lib/LIMS2/Model/Profile/Plate.pm
+++ packages/LIMS2-Model/lib/LIMS2/Model/Profile/Plate.pm
@@ -77,14 +77,65 @@ sub _get_plate_data {
 
 sub process_default_well {
     my ( $self, $well ) = @_;
-    my %well_data;
 
-    $well_data{well_name} = $well->well_name;
+    my $well_data = $well->as_hash;
     my $process_pipeline = $well->process->process_pipeline;
-    $well_data{pipeline} = $process_pipeline ? $process_pipeline->pipeline->pipeline_name : '';
+    $well_data->{pipeline} = $process_pipeline ? $process_pipeline->pipeline->pipeline_name : '';
 
-    return \%well_data;
+    return $well_data;
 }
+
+sub get_well_assay_results {
+    my ( $self, $well ) = @_;
+    my $assay_results;
+    for my $assay_result ( $well->well_assay_results->all ) {
+        $assay_results .= $assay_result->assay . ' - ' . $assay_result->result . "\n";
+    }
+    return $assay_results;
+}
+
+sub get_legacy_qc_results {
+    my ( $self, $well ) = @_;
+    my $legacy_qc_resultset = $well->well_legacy_qc_test_result;
+    return unless $legacy_qc_resultset;
+
+    my $legacy_qc_results;
+    for my $qc_result ( $legacy_qc_resultset->all ) {
+        $legacy_qc_results .= $qc_result->valid_primers . ' - ' . $qc_result->pass_level . "\n";
+    }
+
+    return $legacy_qc_results;
+}
+
+sub get_process_of_type {
+    my ( $self, $process, $type ) = @_;
+
+    if ( $process->process_type->process_type eq $type ) {
+        my $process_type = 'process_' . $type;
+        return $process->$process_type;
+    }
+
+    if ( $process->process_type->process_type eq 'rearray' ) {
+        my @source_processes = map { $_->source_well->process }
+            $process->process_rearray->process_rearray_source_wells_rs->search(
+                {}, { prefetch => { source_well => 'process' } } );
+
+        my @source_params = map { synthetic_construct_params( $_ ) } @source_processes;
+        my $params = shift @source_params;
+        for my $other_params ( @source_params ) {
+            if ( ! Compare( $params, $other_params ) ) {
+                LIMS2::Model::Error::Database->throw(
+                    'Rearray process ' . $process->process_id
+                    . ' has source wells containing different constructs'
+                );
+            }
+        }
+        $self->get_process_of_type( shift @source_processes, $type );
+    }
+
+    return;
+}
+
 1;
 
 __END__
