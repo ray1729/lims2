8e28afe40bbe2e4450c7c01a7c6c9da1952af467
Ray Miller <rm7@sanger.ac.uk>
2012-03-01 14:25:26 +0000

    Implement methods to fetch eng seq params (not the sequence itself), and fix the exception handling for the non-REST methods

--- packages/LIMS2-WebApp/lib/LIMS2/WebApp/Controller/API/EngineeredSequences.pm
+++ packages/LIMS2-WebApp/lib/LIMS2/WebApp/Controller/API/EngineeredSequences.pm
@@ -4,6 +4,7 @@ use HTTP::Status qw( :constants );
 use IPC::System::Simple qw( systemx );
 use File::Temp;
 use Path::Class;
+use Try::Tiny;
 use namespace::autoclean;
 
 BEGIN {extends 'LIMS2::Catalyst::Controller::REST'; }
@@ -20,14 +21,65 @@ Catalyst Controller.
 
 =cut
 
-sub eng_seq :Path( '/api/eng_seq' ) :Args(2) :ActionClass('REST') {
+sub eng_seq_params_for_well :Path( '/api/eng_seq_params' ) :Args(2) :ActionClass( 'REST' ) {
 }
 
-sub eng_seq_GET {
+sub eng_seq_params_for_well_GET {
     my ( $self, $c, $plate_name, $well_name ) = @_;
 
-    my $synvec = $c->model( 'Golgi' )->retrieve_synthetic_construct( { plate_name => $plate_name, well_name => $well_name } );
+    my $entity = $c->model( 'Golgi' )->retrieve_synthetic_construct_params(
+        {
+            plate_name => $plate_name,
+            well_name  => $well_name
+        }
+    );
 
+    $self->status_ok( $c, entity => $entity );
+}
+
+sub eng_seq_params_for_plate :Path( '/api/eng_seq_params' ) :Args(1) :ActionClass( 'REST' ) {
+}
+
+sub eng_seq_params_for_plate_GET {
+    my ( $self, $c, $plate_name ) = @_;
+
+    my $plate = $c->model( 'Golgi' )->retrieve( Plate => { plate_name => $plate_name }, { prefetch => 'wells' } );
+
+    my @params;
+
+    for my $well ( $plate->wells ) {
+        push @params, $c->model( 'Golgi' )->retrieve_synthetic_construct_params(
+            {
+                plate_name => $plate_name,
+                well_name  => $well->well_name
+            }
+        );        
+    }
+
+    my $entity;
+    
+    if ( $c->req->param( 'unique' ) ) {
+        $entity = $self->_deep_uniq( \@params );
+    }
+    else {
+        $entity = \@params;
+    }    
+
+    $self->status_ok( $c, entity => $entity );
+}
+
+sub eng_seq_for_well :Path( '/api/eng_seq' ) :Args(2) {
+    my ( $self, $c, $plate_name, $well_name ) = @_;
+
+    my $synvec;
+    try {
+        $synvec = $c->model( 'Golgi' )->retrieve_synthetic_construct( { plate_name => $plate_name, well_name => $well_name } );
+    }
+    catch {
+        $c->error( $_ );
+        $c->detach( 'handle_error' );
+    };
+        
     my $format = $c->request->param( 'format' ) || 'genbank';
     
     $c->response->body( $synvec->formatted_seq( $format ) );
@@ -35,7 +87,7 @@ sub eng_seq_GET {
     $c->response->status( HTTP_OK );
 }
 
-sub eng_seqs :Path( '/api/eng_seq' ) :Args(1) {
+sub eng_seqs_for_plate :Path( '/api/eng_seq' ) :Args(1) {
 }
 
 =head2 GET /api/eng_seq/$plate_name
@@ -44,40 +96,40 @@ Retrieve a compressed tarball containing all of the engineered sequences for C<$
 
 =cut
 
-sub eng_seqs_GET {
+sub eng_seqs_for_plate_GET {
     my ( $self, $c, $plate_name ) = @_;
 
-    my $plate = $c->model( 'Golgi' )->retrieve( Plate => { plate_name => $plate_name }, { prefetch => 'wells' } );
-
-    my $format = $c->request->param( 'format' ) || 'genbank';
+    my $tarball;
     
-    my $tempdir = File::Temp->newdir;
-    my $outdir  = dir( $tempdir->dirname )->subdir( $plate );
+    try {
+        my $plate = $c->model( 'Golgi' )->retrieve( Plate => { plate_name => $plate_name }, { prefetch => 'wells' } );
 
-    my %seen;
+        my $format = $c->request->param( 'format' ) || 'genbank';
     
-    for my $well ( $plate->wells ) {
-        my $synvec  = $c->model( 'Golgi' )->retrieve_synthetic_construct( { plate_name => $plate->plate_name, well_name => $well->well_name } );
-        next if $seen{ $synvec->display_id }++;
-        my $outfile = $outdir->file( $synvec->display_id . '.gbk' );
-        my $seq_io = Bio::SeqIO->new( -file => $outfile, -format => $format );
-        $seq_io->write_seq( $synvec->bio_seq );
+        my $tempdir = File::Temp->newdir;
+        my $outdir  = dir( $tempdir->dirname )->subdir( $plate );
+
+        my %seen;
+    
+        for my $well ( $plate->wells ) {
+            my $synvec  = $c->model( 'Golgi' )->retrieve_synthetic_construct( { plate_name => $plate->plate_name, well_name => $well->well_name } );
+            next if $seen{ $synvec->display_id }++;
+            my $outfile = $outdir->file( $synvec->display_id . '.gbk' );
+            my $seq_io = Bio::SeqIO->new( -file => $outfile, -format => $format );
+            $seq_io->write_seq( $synvec->bio_seq );
+        }
+
+        $tarball = $tempdir->file( $plate->plate_name . '.tar.gz' );
+        systemx( 'tar', '-z', '-c', '-f', $tarball, $outdir );
     }
-
-    my $tarball = $tempdir->file( $plate->plate_name . '.tar.gz' );
-    systemx( 'tar', '-z', '-c', '-f', $tarball, $outdir );
+    catch {
+        $c->error( $_ );
+        $c->detach( 'handle_error' );
+    };    
 
     $c->response->body( $tarball->openr );
     $c->response->header( 'Content-Type' => 'application/x-tar' ); # XXX is this the correct MIME type for compressed tar?
     $c->response->status( HTTP_OK );
-}
-
-sub end :Private {
-    my ( $self, $c ) = @_;
-
-    if ( @{$c->error} ) {
-        $c->forward( 'handle_error' );
-    }    
 }
 
 =head1 AUTHOR
